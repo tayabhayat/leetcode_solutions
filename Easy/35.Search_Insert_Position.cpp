ac1c75b768394d2a2cd68b9b7be3bc3f7ab59950
Tayyab Hayat <tayabhayat36@gmail.com>
2026-06-20 17:37:09 +0500

    35.Search_Insert_Position

---
+++ Easy/35.Search_Insert_Position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = (start + end) / 2;
+
+        while(start <= end){
+            mid = (start + end) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+            return start;
+    }
+};
