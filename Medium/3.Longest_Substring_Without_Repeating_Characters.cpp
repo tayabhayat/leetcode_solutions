5653763f9714535801e69e31deb9d239b5fd667b
Tayyab Hayat <tayabhayat36@gmail.com>
2026-08-01 22:10:44 +0500

    3. Longest Substring Without repeating characters

---
+++ Medium/3.Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> window;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            while (window.count(s[right])) {
+                window.erase(s[left]);
+                left++;
+            }
+
+            window.insert(s[right]);
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
