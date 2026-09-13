a6c9ba07e6b0c5e7731a1b954859675ea087bf43
Tayyab Hayat <tayabhayat36@gmail.com>
2026-09-13 18:51:47 +0500

    Two sum better approach

--- Easy/1.Two_Sum.cpp
+++ Easy/1.Two_Sum.cpp
@@ -10,3 +10,35 @@ public:
         return{};
     }
 };
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> arr;
+
+        for (int i = 0; i < nums.size(); i++) {
+            arr.push_back({nums[i], i});
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int i = 0;
+        int j = arr.size() - 1;
+
+        while (i < j) {
+            int sum = arr[i].first + arr[j].first;
+
+            if (sum == target) {
+                return {arr[i].second, arr[j].second};
+            }
+            else if (sum > target) {
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+
+        return {};
+    }
+};
