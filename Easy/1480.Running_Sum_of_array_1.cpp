54c6c258185276a502972f910d503c903b4cf306
Tayyab Hayat <tayabhayat36@gmail.com>
2026-09-21 23:28:44 +0500

    Running sum 1480

--- Easy/1480.Running_Sum_of_array_1.cpp
+++ Easy/1480.Running_Sum_of_array_1.cpp
@@ -7,3 +7,12 @@ public:
         return nums;
     }
 };
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+       for(int i = 1; i < nums.size(); i++){
+        nums[i] += nums[i - 1];
+       }
+       return nums;
+    }
+};
